db8faf330c9b3b89da950bb9b8badb53b2aebb39
quinjenn <jquintal_@hotmail.com>
2023-04-04 10:08:54 -0700

    git add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Total revenue for the entire company (all stores)
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue: #{total_revenue}"
+
+# Average annual revenue for all stores
+avg_annual_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue: #{avg_annual_revenue}"
+
+#Calculations
+# Number of stores that are generating $1M or more in annual sales
+num_stores = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of stores generating $1M or more in annual sales: #{num_stores}"
