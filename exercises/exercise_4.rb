2ca7654a44af7fdf7f7e3b75d0382625146e47f3
quinjenn <jquintal_@hotmail.com>
2023-04-04 10:05:45 -0700

    git add exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,21 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+#Loading a subset of stores
+# Creating 3 more stores
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetching stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "Name: #{store.name} | Annual Revenue: #{store.annual_revenue}"
+end
+
+# Fetching stores that carry women's apparel and generate less than $1M in annual revenue
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+@womens_stores.each do |store|
+  puts "Name: #{store.name} | Annual Revenue: #{store.annual_revenue}"
+end
