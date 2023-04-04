f5180ffc922837a8904143446dda48d4e7fddc4b
quinjenn <jquintal_@hotmail.com>
2023-04-04 09:54:39 -0700

    git add first exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Create three stores
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true)
+
+# Output the number of stores
+puts Store.count
